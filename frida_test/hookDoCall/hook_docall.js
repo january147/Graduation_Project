689247bb9dbf313928c22b94ee867a5596a817bf
january <january147@outlook.com>
2019-04-11 20:25:09 +0800

    backup Apr 11th

--- frida_test/hookDoCall/hook_docall.js
+++ frida_test/hookDoCall/hook_docall.js
@@ -17,17 +17,58 @@ function hookFunctionByName(module, func){
         }
     })
 }
+//读取String类型字符串
+function readStdString (str) {
+    const isTiny = (Memory.readU8(str) & 1) === 0;
+    if (isTiny) {
+      return Memory.readUtf8String(str.add(1));
+    }
+  
+    return Memory.readUtf8String(Memory.readPointer(str.add(2 * Process.pointerSize)));
+  }
 
-hookFunctionByName('libc.so','write')
-//listFunction('libc++.so');
+function getPrettyMethod(){
+    pretty_method_funcs = ['_ZN3art9ArtMethod12PrettyMethodEPS0_b', '_ZNK3art7DexFile12PrettyMethodEjb', '_ZN3art9ArtMethod12PrettyMethodEb'];
+    lib = 'libart.so';
+    pretty_method_address = Module.findExportByName(lib, pretty_method_funcs[0]);
+    pretty_method = new NativeFunction(pretty_method_address, 'pointer', ['pointer', 'pointer', 'bool'])
+    return pretty_method
+}
+
+function hook(){
+    invoke_address = Module.findExportByName('libart.so','_ZN3art9ArtMethod6InvokeEPNS_6ThreadEPjjPNS_6JValueEPKc')
+    pretty_method = getPrettyMethod()
+    Interceptor.attach(invoke_address, {
+        onEnter:function(args){
+            send('ArtMethod is at '+args[0])
+            var method = pretty_method(args[0], args[0], 1);
+            send(readStdString(method))
+        },
+        onLeave:function(retval){
+            
+        }
+
+    });
+/*
+    do_call_funcs = ['_ZN3art11interpreter6DoCallILb1ELb0EEEbPNS_9ArtMethodEPNS_6ThreadERNS_11ShadowFrameEPKNS_11InstructionEtPNS_6JValueE',
+'_ZN3art11interpreter6DoCallILb1ELb1EEEbPNS_9ArtMethodEPNS_6ThreadERNS_11ShadowFrameEPKNS_11InstructionEtPNS_6JValueE'];
+    var docall = Module.findExportByName('libart.so', do_call_funcs[0]);
+    Interceptor.attach(docall, {
+        onEnter:function(args){
+            send('ArtMethod is at '+args[0])
+            //var method = pretty_method(args[0], args[0], 1);
+            //send(readStdString(method))
+        },
+    });
+*/
+}
+hook()
 
 /*
 do_call_funcs = ['_ZN3art11interpreter6DoCallILb1ELb0EEEbPNS_9ArtMethodEPNS_6ThreadERNS_11ShadowFrameEPKNS_11InstructionEtPNS_6JValueE',
 '_ZN3art11interpreter6DoCallILb1ELb1EEEbPNS_9ArtMethodEPNS_6ThreadERNS_11ShadowFrameEPKNS_11InstructionEtPNS_6JValueE'];
 
 pretty_method_funcs = ['_ZN3art9ArtMethod12PrettyMethodEPS0_b', '_ZNK3art7DexFile12PrettyMethodEjb', '_ZN3art9ArtMethod12PrettyMethodEb'];
-
-notice = ['docall0', 'docall1'];
 
 lib = 'libart.so';
 
@@ -44,21 +85,17 @@ if(docall != null){
     send('no func');
 }
 
-function readStdString (str) {
-  const isTiny = (Memory.readU8(str) & 1) === 0;
-  if (isTiny) {
-    return Memory.readUtf8String(str.add(1));
-  }
 
-  return Memory.readUtf8String(Memory.readPointer(str.add(2 * Process.pointerSize)));
-}
+
+printf_address = Module.findExportByName('libc.so', 'printf')
+printf = new NativeFunction(printf_address, 'pointer', ['pointer'])
+msg = Memory.allocUtf8String('hello\n')
 
 Interceptor.attach(docall, {
     onEnter:function(args){
         send('arg0 ' + args[0].toString());
-        var method = pretty_method(args[0], ptr(args[0]), 0);
+        var method = pretty_method(args[0], args[0], 0);
         send(readStdString(method))
-        Thread.sleep(2)
     },
     onLeave:function(args){
         send('docall end');
